e85d71a24c8484ce681be090d69b170dc57924e5
Turbo Ninh <me@turboninh.com>
2023-09-18 15:48:42 +0200

    optimize script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 
 for dir in ./containers/*; do
   if [ "$dir" != "./containers/.unused" ] && [ -d "$dir" ] && [ -f "$dir/docker-compose.yml" ]; then
-    docker compose up -d -f "$dir/docker-compose.yml"
-    echo "Started $dir"
+    docker compose down -f "$dir/docker-compose.yml"
+    echo "Stopped $dir"
   fi
 done
