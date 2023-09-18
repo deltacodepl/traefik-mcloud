2a3be4e20520d3ffb955776a0959e9f17a2799b5
Turbo Ninh <me@turboninh.com>
2023-09-18 16:04:36 +0200

    fix script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 for dir in ./containers/*; do
   if [ "$dir" != "./containers/.unused" ] && [ -d "$dir" ] && [ -f "$dir/docker-compose.yml" ]; then
-    docker compose up -d -f "$dir/docker-compose.yml"
+    docker compose -f "$dir/docker-compose.yml" up -d
     echo "Started $dir"
   fi
 done
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 
 for dir in ./containers/*; do
   if [ "$dir" != "./containers/.unused" ] && [ -d "$dir" ] && [ -f "$dir/docker-compose.yml" ]; then
-    docker compose down -f "$dir/docker-compose.yml"
+    docker compose -f "$dir/docker-compose.yml" down
     echo "Stopped $dir"
   fi
 done
